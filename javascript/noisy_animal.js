4c9c468c3b8ea641a8ec9930ca78212b7dc0134a
Joe Grossberg <josephgrossberg+github@gmail.com>
2025-03-07 16:32:40 -0800

    notes to self

--- javascript/noisy_animal.js
+++ javascript/noisy_animal.js
@@ -1,3 +1,11 @@
+// When/If you're feeling comfortable with the code, try adding these animals:
+
+// A snake that makes the noise "hiss" when it is loud and "slither" when it is not loud
+// A mouse that is so silent that it outputs exactly one blank line, whether loud or quiet
+// Support the true or false values for the is_loud flag in both cases.
+
+// Don't forget the tests for your new requirements! Don't change the content of any existing tests, although you can reorganise the order and the context and describe blocks if it is helpful.
+
 function createNoisyAnimal(species) {
   const animalNoiseMap = {
     owl: 'hoot',
